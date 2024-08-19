181aa2a0869502fc93682308f956f5b0064a0fc7
Arne Styve <asty@ntnu.no>
2024-08-19 10:14:37 +0200

    Added the project Chapter01->house.

---
+++ maven/projects/chapter01/house/src/main/java/Main.java
@@ -0,0 +1,18 @@
+/**
+ * The sole purpose of this Main class is to run the program. All operating
+ * systems expects to find one single method in the code named "main" that
+ * takes an array of strings as a parameter and returns nothing.
+ * This method is then used to start your application.
+ */
+public class Main {
+
+  /**
+   * The main method is the entry point of the program.
+   *
+   * @param args Any "arguments" provided from the commandline upon starting the program.
+   */
+  public static void main(String[] args) {
+    Picture picture = new Picture();
+    picture.draw();
+  }
+}
